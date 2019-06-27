86b4ceaae3c65186636e7b2c338774f9f776d22e
hiramoto-kensuke <hiramoto.kensuke@moneyforward.co.jp>
2019-06-28 01:51:13 +0900

    release直前

---
+++ lib/saikouda.rb
@@ -0,0 +1,7 @@
+require "saikouda/version"
+require "saikouda/cli"
+
+module Saikouda
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/saikouda/version.rb
@@ -0,0 +1,3 @@
+module Saikouda
+  VERSION = "0.1.0"
+end
